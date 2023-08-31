8e97d540b422c71390051e4ae83039d1769ebe71
Jonas Peres <jonasperes10@hotmail.com>
2023-08-31 10:13:54 -0300

    New setup with docs

---
+++ docs/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import '@squidit/css/dist/css/squid.min.css'
+
+createApp(App).mount('#app')
--- src/src/components/main.ts
+++ src/src/components/main.ts
@@ -9,4 +9,4 @@ export default {
   }
 };
 
-export { InputSquid };
+export { ButtonSquid, InputSquid, LoaderSquid };
