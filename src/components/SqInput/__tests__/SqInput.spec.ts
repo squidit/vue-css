73523890597df4112f0b7a1b7b023174ad3df003
JonasPeres <jonasperes10@hotmail.com>
2024-06-11 15:15:22 -0300

    Fix SqInput test

--- src/components/SqInput/__tests__/SqInput.spec.ts
+++ src/components/SqInput/__tests__/SqInput.spec.ts
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils'
 import { SqInput } from '@components/index'
+import i18n from '@src/i18n'
 
 describe('SqInput', () => {
   it('Should Render', () => {
-    const mountLoader = mount(SqInput)
+    const mountLoader = mount(SqInput, { global: { plugins: [i18n] } })
     expect(mountLoader).toBeTruthy()
   })
 })
