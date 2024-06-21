5dd4bece462605e64a1305595aa3b356d71e227a
JonasPeres <jonasperes10@hotmail.com>
2024-06-21 01:00:06 -0300

    Fix tests

--- src/components/SqSelectMultiTags/__tests__/SqSelectMultiTags.spec.ts
+++ src/components/SqSelectMultiTags/__tests__/SqSelectMultiTags.spec.ts
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils'
 import { SqSelectMultiTags } from '@components/index'
+import VueCssPlugin from '@src/vue-css'
 
 describe('SqSelectMultiTags', () => {
   it('Should Render', () => {
-    const mountLoader = mount(SqSelectMultiTags)
+    const mountLoader = mount(SqSelectMultiTags, { global: { plugins: [VueCssPlugin] } })
     expect(mountLoader).toBeTruthy()
   })
 })
--- src/components/SqSelector/__tests__/SqSelector.spec.ts
+++ src/components/SqSelector/__tests__/SqSelector.spec.ts
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils'
 import { SqSelector } from '@components/index'
+import VueCssPlugin from '@src/vue-css'
 
 describe('SqSelector', () => {
   it('Should Render', () => {
-    const mountLoader = mount(SqSelector)
+    const mountLoader = mount(SqSelector, { global: { plugins: [VueCssPlugin] } })
     expect(mountLoader).toBeTruthy()
   })
 })
